9b1ee1f823595d3d7076c4dce3b5ab4a50fb47e7
Sandeep Sarkar <152861071+sandy4242@users.noreply.github.com>
2024-12-15 10:35:34 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Lingua', 
+    version='0.1.0',  
+    author='Sandeep Sarkar',  
+    author_email='your.email@example.com', 
+    description='This library is created to write codes in HinGlish(Hindi & English)',
+        url='https://github.com/sandy4242/Lingua',
+    packages=find_packages(), 
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License', 
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6', 
+  
+)
